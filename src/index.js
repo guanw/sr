a4ac670ed9a799226e2c36d1150cb837a3f284d2
guanw <guanw0826@gmail.com>
2024-07-04 17:46:48 -0400

    init the pixi js project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as PIXI from "pixi.js";
+
+(async () =>
+    {
+        // Create a PixiJS application.
+        const app = new PIXI.Application();
+
+        // Intialize the application.
+        await app.init({ background: '#1099bb', resizeTo: window });
+
+        // Then adding the application's canvas to the DOM body.
+        document.body.appendChild(app.canvas);
+    })();
