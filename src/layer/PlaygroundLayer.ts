030f7c72113015fd5f8b82d38f6f780bc0b740a3
guanw <guanw0826@gmail.com>
2024-08-04 14:37:24 -0400

    playground - make slime animated sprite work with gh pages

--- src/layer/PlaygroundLayer.ts
+++ src/layer/PlaygroundLayer.ts
@@ -11,16 +11,33 @@ export class PlaygroundLayer {
     public static async genInstance(): Promise<PlaygroundLayer> {
         if (!PlaygroundLayer.instance) {
             PlaygroundLayer.instance = new PlaygroundLayer();
+
+            await PlaygroundLayer.createAnimatedSlime()
         }
-        const circle = new PIXI.Graphics();
-        circle.beginFill(0x9966FF);
-        circle.drawCircle(0, 0, 50);
-        circle.endFill();
-        circle.x = 400;
-        circle.y = 300;
-        PlaygroundLayer.instance.layer.addChild(circle);
+
         return PlaygroundLayer.instance;
       }
+
+    private static async createAnimatedSlime() {
+        const texture = await PIXI.Assets.load('https://guanw.github.io/sr_assets/slime_run.png');
+        const frames = [];
+        const frameWidth = 32; // Width of a single frame in pixels
+        const frameHeight = 32; // Height of a single frame in pixels
+        const numberOfFrames = 6; // Number of frames in the animation
+        for (let i = 0; i < numberOfFrames; i++) {
+            const rect = new PIXI.Rectangle(i * frameWidth, 0, frameWidth, frameHeight);
+            frames.push(new PIXI.Texture({source: texture.baseTexture, frame: rect}));
+        }
+        // Create an AnimatedSprite with the frames
+        const animatedSlime = new PIXI.AnimatedSprite(frames);
+        // Set animation speed (frames per second)
+        animatedSlime.animationSpeed = 0.1;
+        // Start the animation
+        animatedSlime.play();
+        animatedSlime.x = 400;
+        animatedSlime.y = 300;
+        PlaygroundLayer.instance.layer.addChild(animatedSlime);
+    }
 
     async update() {
 
